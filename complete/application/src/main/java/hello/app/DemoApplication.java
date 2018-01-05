b7ea4fa405891ffa5ccdbcdc3f580e48a7584c22
Greg Turnquist <gturnquist@pivotal.io>
2018-01-05 11:59:43 -0600

    Polish

--- complete/application/src/main/java/hello/app/DemoApplication.java
+++ complete/application/src/main/java/hello/app/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "hello", scanBasePackageClasses = )
+@SpringBootApplication(scanBasePackages = "hello")
 @RestController
 public class DemoApplication {
 

8edf40f9ca12b109c396a2bde5c2f94b8eea17ba
Jay Bryant <jbryant@pivotal.io>
2019-09-26 13:02:47 -0500

    Use the Spring Initializr
    I rewrote the document and the code (mostly changing package names) to use the Spring Initializr. This particular example involved modifying both the Maven and the Gradle build files, which added quite a bit to the document.

--- complete/application/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
+++ complete/application/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
@@ -1,28 +1,25 @@
-package hello.service;
+package com.example.multimodule.application;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.multimodule.service.MyService;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest("service.message=Hello")
-public class MyServiceTest {
+@SpringBootTest
+public class DemoApplicationTest {
 
     @Autowired
     private MyService myService;
 
     @Test
     public void contextLoads() {
-        assertThat(myService.message()).isNotNull();
-    }
-
-    @SpringBootApplication
-    static class TestConfiguration {
+        assertThat(myService).isNotNull();
     }
 
 }
--- complete/library/src/test/java/com/example/multimodule/service/MyServiceTest.java
+++ complete/library/src/test/java/com/example/multimodule/service/MyServiceTest.java
@@ -1,25 +1,28 @@
-package hello.app;
+package com.example.multimodule.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import hello.service.MyService;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoApplicationTest {
+@SpringBootTest("service.message=Hello")
+public class MyServiceTest {
 
     @Autowired
     private MyService myService;
 
     @Test
     public void contextLoads() {
-        assertThat(myService).isNotNull();
+        assertThat(myService.message()).isNotNull();
+    }
+
+    @SpringBootApplication
+    static class TestConfiguration {
     }
 
 }
